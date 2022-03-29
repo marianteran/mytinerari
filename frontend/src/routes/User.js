bbe3de9c8662f9fd0871d338d3c5c408c28156c6
marianteran <marianteranf@gmail.com>
2022-03-29 14:41:17 -0300

    add content user

--- frontend/src/routes/User.js
+++ frontend/src/routes/User.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from 'axios';
+import { Link as LinkRouter } from "react-router-dom";
 
 
 import it1 from '../imagen/datouser/arraialcaboriojaneiro1.jpg'
@@ -11,7 +12,7 @@ import it6 from '../imagen/datouser/athensHighlights3.jpg'
 
 import { useStateValue } from '../core/context/StateProvider';
 
-
+import avatar from "../imagen/datouser/avatar1.png"
 
 
 const DatosUser = () => {
@@ -23,8 +24,21 @@ const DatosUser = () => {
 	//console.log(usuario)
 
 	useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+		window.scrollTo(0, 0);
+	}, []);
+
+	const cargarImagen = () => {
+		if (user.datosUser.imguser === "") {
+			return avatar
+
+		} else {
+			return user.datosUser.imguser
+		}
+
+
+	}
+
+
 
 	return (
 		< div className='usuario'>
@@ -37,8 +51,9 @@ const DatosUser = () => {
 
 					<div className='container content-marco'>
 						<div className='marco'>
-							<img src={user.datosUser.imguser} alt='user'></img>
-							{/*  {user ? <img className="logo" src={user.datosUser.imguser} alt="user"/> : <img src={avatar} alt='user'></img>  }   */}
+							<img src={cargarImagen()} alt='user'></img>
+							{/* {user? <img className="logo" src={cargarImagen()} alt="user"/> : <img src={avatar} alt='user'></img>  } */}
+
 						</div>
 						<div className='marco-info'>
 							<h5 style={{ textTransform: "uppercase" }}> {user.datosUser.firstname}</h5>
@@ -50,8 +65,75 @@ const DatosUser = () => {
 					<div className='subtitle-principal-user '>
 						<h2>Start your Travel</h2>
 					</div>
+
+					<div className="heroButtons">
+						<LinkRouter to="/cities">
+							<button>LET'S  GO</button>
+						</LinkRouter>
+					</div>
 				</div>
 			</div>
+
+
+
+
+
+
+
+
+			<div className='main-datosUser'>
+
+
+				<div className='content-info-user-card'>
+
+					<div className='content-user-card '>
+
+						<div className='content-user-info-img'>
+							<img src="./assets/aventure.jpg" alt="mountain" ></img>
+						</div>
+
+
+						<div className='content-card-user-info'>
+							<p>No matter where you want to go, discover your destination.</p>
+
+						</div>
+
+					</div>
+
+
+					<div className='content-user-card'>
+
+						<div className='content-user-info-img'>
+							<img src="./assets/outdoor.jpg" alt="mountain" ></img>
+						</div>
+
+						<div className='content-card-user-info'>
+							<p>Maximize your vacation experience just in the right way.</p>
+
+						</div>
+
+					</div>
+
+
+					<div className='content-user-card'>
+
+						<div className='content-user-info-img'>
+							<img src="./assets/mountain.jpg" alt="mountain" ></img>
+						</div>
+
+						<div className='content-card-user-info'>
+							<p>Every Mountain top is within reach if you just keep climbing.</p>
+
+						</div>
+					</div>
+
+				</div>
+
+
+
+			</div>
+
+
 
 
 
@@ -169,3 +251,7 @@ const DatosUser = () => {
 }
 
 export default DatosUser
+
+
+
+
